4c9f46b9041cdc85debb662c365d781adb220218
Bram Oosterlynck <booster@inuits.eu>
2021-03-15 18:43:41 +0100

    Add CORS headers for download mediafile

--- resources/download.py
+++ resources/download.py
@@ -1,6 +1,6 @@
 import os
 from flask_restful import Resource
-from flask import send_file
+from flask import send_file, after_this_request
 from flask_restful_swagger import swagger
 
 from storage.storage import download_file
@@ -12,4 +12,8 @@ class Download(Resource):
     @app.oidc.accept_token(require_token=True, scopes_required=['openid'])
     def get(self, filename):
         output = download_file(filename)
-        return send_file(output, as_attachment=True)
+        @after_this_request
+        def add_header(response):
+            response.headers['Access-Control-Allow-Origin'] = '*'
+            return response
+        return send_file(output)
