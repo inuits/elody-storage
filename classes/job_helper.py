5f4d273ea02575995aabb9c831eaa4a38ecf063e
Matthias Dillen <matthias.dillen@inuits.eu>
2021-08-31 16:04:35 +0200

    Refs #81563 add job helper

--- classes/job_helper.py
+++ classes/job_helper.py
@@ -1,9 +1,7 @@
 import json
 import string
-import pika
 from datetime import datetime
 from enum import Enum
-
 import requests
 
 
@@ -15,20 +13,16 @@ class Status(Enum):
 
 
 class JobHelper:
-    def __init__(self, job_api_base_url, amqp_url, amqp_exchange_name, amqp_queue_name, amqp_routing_key):
+    def __init__(self, job_api_base_url):
         self.job_api_base_url = job_api_base_url
-        amqp_connection = pika.BlockingConnection(pika.ConnectionParameters(amqp_url))
-        self.amqp_channel = amqp_connection.channel()
-        self.amqp_channel.queue_declare(queue=amqp_queue_name)
-        self.amqp_exchange_name = amqp_exchange_name
-        self.amqp_routing_key = amqp_routing_key
 
     def __patch_job(self, job):
         return requests.patch(
             "{}/jobs/{}".format(self.job_api_base_url, job["_id"]), json=job
         ).json()
 
-    def create_new_job(self, job_info: string, job_type: string, user: string, asset_id=None, mediafile_id=None, parent_job_id=None):
+    def create_new_job(self, job_info: string, job_type: string, user: string, asset_id=None, mediafile_id=None,
+                       parent_job_id=None):
         new_job = {
             "job_type": job_type,
             "job_info": job_info,
@@ -51,22 +45,12 @@ class JobHelper:
         job["status"] = Status.IN_PROGRESS.value
         return self.__patch_job(job)
 
-    def finish_job(self, job, message=""):
+    def finish_job(self, job):
         job["status"] = Status.FINISHED.value
         job["end_time"] = str(datetime.utcnow())
-        self.amqp_channel.basic_publish(
-            exchange=self.amqp_exchange_name,
-            routing_key=self.amqp_routing_key,
-            body=bytes("Job with id {} finished: {}".format(job["_id"], message))
-        )
         return self.__patch_job(job)
 
-    def fail_job(self, job, error_message=""):
+    def fail_job(self, job):
         job["status"] = Status.FAILED.value
         job["end_time"] = str(datetime.utcnow())
-        self.amqp_channel.basic_publish(
-            exchange=self.amqp_exchange_name,
-            routing_key=self.amqp_routing_key,
-            body=bytes("Job with id {} failed: {}".format(job["_id"], message))
-        )
         return self.__patch_job(job)
