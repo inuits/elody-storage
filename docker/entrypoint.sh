163deb84a561d94279f3578483113e0ab31c1098
Wouter D'Haeseleer <wodh@inuits.eu>
2021-12-03 07:58:04 +0000

    Adding option to install extra CA's

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,6 +8,12 @@ if [ ! -z "$@" ]; then
   exit $?
 fi
 
+if [ ! -z "$TRUSTED_CA_BUNDLE" ]; then
+  cp /etc/ssl/certs/ca-certificates.crt /tmp/ca-certificates.crt
+  echo "${TRUSTED_CA_BUNDLE}" >> /tmp/ca-certificates.crt
+  export CURL_CA_BUNDLE="/tmp/ca-certificates.crt"
+fi
+
 if [ "$APP_ENV" = "dev" ]; then
   echo "Starting development server..."
   export FLASK_ENV=development
