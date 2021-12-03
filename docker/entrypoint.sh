c689094d91396b7d3f46e24086737ee1f172f93b
Wouter D'Haeseleer <wodh@inuits.eu>
2021-12-03 16:28:17 +0000

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -13,7 +13,7 @@ if [ ! -z "$TRUSTED_CA_BUNDLE" ]; then
   cp /etc/ssl/certs/ca-certificates.crt /tmp/ca-certificates.crt
   echo "${TRUSTED_CA_BUNDLE}" >> /tmp/ca-certificates.crt
   export CURL_CA_BUNDLE="/tmp/ca-certificates.crt"
-  GUNICORN_SSL_CA==" --ca-certs /tmp/ca-certificates.crt"
+  GUNICORN_SSL_CA=" --ca-certs /tmp/ca-certificates.crt"
 fi
 
 if [ "$APP_ENV" = "dev" ]; then
