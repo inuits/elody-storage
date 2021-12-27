80a10a570ea18632c13e82a18de53032145115b0
Gijs Vermeulen <gverm@inuits.eu>
2021-12-27 21:35:39 +0100

    Simplify uploading with key

--- api/resources/upload.py
+++ api/resources/upload.py
@@ -20,23 +20,7 @@ def _get_mediafile_id(req):
 
 class Upload(Resource):
     @app.require_oauth()
-    def post(self):
-        job = job_helper.create_new_job("Upload file", "file_upload")
-        job_helper.progress_job(job)
-        try:
-            file = request.files["file"]
-            mediafile_id = _get_mediafile_id(request)
-            upload_file(file, mediafile_id)
-            job_helper.finish_job(job)
-        except Exception as ex:
-            job_helper.fail_job(job, str(ex))
-            return str(ex), 400
-        return "", 201
-
-
-class UploadKey(Resource):
-    @app.require_oauth()
-    def post(self, key):
+    def post(self, key=None):
         job = job_helper.create_new_job("Upload file", "file_upload")
         job_helper.progress_job(job)
         try:
@@ -48,3 +32,9 @@ class UploadKey(Resource):
             job_helper.fail_job(job, str(ex))
             return str(ex), 400
         return "", 201
+
+
+class UploadKey(Upload):
+    @app.require_oauth()
+    def post(self, key):
+        return super().post(key)
