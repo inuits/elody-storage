e5f1611910369ce47536140dfe8f2524e9fe4ab7
Bram Oosterlynck <booster@inuits.eu>
2022-05-02 14:37:24 +0200

    Add FLASK_ENV in run-tests script and run moto on other port then 5000

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -2,10 +2,11 @@
 
 export PATH=${PATH}:/app/.local/bin
 export REQUIRE_TOKEN=0
-export MINIO_ENDPOINT=http://localhost:5000
+export MINIO_ENDPOINT=http://localhost:3000
 export MINIO_BUCKET=test
 export MINIO_ACCESS_KEY=test
 export MINIO_SECRET_KEY=test
+export FLASK_ENV=development
 
 cat << EOF
 =========================================
@@ -13,7 +14,7 @@ cat << EOF
 =========================================
 EOF
 
-moto_server s3 > /dev/null 2>&1 &
+moto_server -p3000 s3 > /dev/null 2>&1 &
 coverage run -m pytest -s
 
 cat << EOF
