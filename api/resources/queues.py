2239c6ddce9b3d215adc37be2bbf490092ad7e63
Gijs Vermeulen <gverm@inuits.eu>
2022-12-15 10:31:46 +0100

    Refs #103314 clean up queues and re-enable exif adding

--- api/resources/queues.py
+++ api/resources/queues.py
@@ -5,14 +5,13 @@ from storage.storagemanager import StorageManager
 
 @app.rabbit.queue("dams.file_uploaded")
 def handle_file_uploaded(routing_key, body, message_id):
-    mediafile = body["data"]["mediafile"]
-    if (
-        "mimetype" not in mediafile
-        or "metadata" not in mediafile
-        or not len(mediafile["metadata"])
-    ):
+    data = body["data"]
+    if any(x not in data for x in ["mediafile"]):
+        app.logger.error("Message malformed: missing 'mediafile'")
         return
-    # storage.add_exif_data(mediafile)
+    mediafile = data["mediafile"]
+    if "mimetype" in mediafile and "metadata" in mediafile and mediafile["metadata"]:
+        StorageManager().get_storage_engine().add_exif_data(mediafile)
 
 
 @app.rabbit.queue("dams.file_scanned")
@@ -23,31 +22,32 @@ def remove_infected_file(routing_key, body, message_id):
             "Message malformed: missing 'mediafile_id', 'clamav_version' or 'infected'"
         )
         return
-    if not data["infected"]:
-        return
-    StorageManager().get_storage_engine().delete_files([data["filename"]])
+    if data["infected"]:
+        StorageManager().get_storage_engine().delete_files([data["filename"]])
 
 
 @app.rabbit.queue("dams.mediafile_changed")
 def handle_mediafile_updated(routing_key, body, message_id):
-    old_mediafile = body["data"]["old_mediafile"]
-    mediafile = body["data"]["mediafile"]
+    data = body["data"]
+    if any(x not in data for x in ["old_mediafile", "mediafile"]):
+        app.logger.error("Message malformed: missing 'old_mediafile' or 'mediafile'")
+        return
+    old_mediafile = data["old_mediafile"]
+    mediafile = data["mediafile"]
     if "mimetype" not in mediafile or "metadata" not in mediafile:
         return
-    if (
-        "metadata" in old_mediafile
-        and not StorageManager()
-        .get_storage_engine()
-        .is_metadata_updated(old_mediafile["metadata"], mediafile["metadata"])
+    storage = StorageManager().get_storage_engine()
+    if "metadata" not in old_mediafile or storage.is_metadata_updated(
+        old_mediafile["metadata"], mediafile["metadata"]
     ):
-        return
-    # storage.add_exif_data(mediafile)
+        storage.add_exif_data(mediafile)
 
 
 @app.rabbit.queue("dams.mediafile_deleted")
 def handle_mediafile_deleted(routing_key, body, message_id):
     data = body["data"]
-    if "mediafile" not in data or "linked_entities" not in data:
+    if any(x not in data for x in ["mediafile", "linked_entities"]):
+        app.logger.error("Message malformed: missing 'mediafile' or 'linked_entities'")
         return
     files = [data["mediafile"]["filename"]]
     if "transcode_filename" in data["mediafile"]:
