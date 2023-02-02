72a5d806d9d0af81c60b78fcc8d96a9c53828fb8
Gijs Vermeulen <gverm@inuits.eu>
2023-02-02 14:20:11 +0100

    Remove obsolete test

--- api/tests/test_upload_file.py
+++ api/tests/test_upload_file.py
@@ -9,19 +9,6 @@ from unittest.mock import patch, MagicMock
 @patch("requests.put", new=MagicMock())
 @patch("storage.s3store.S3StorageManager._get_mediafile", new=MagicMock())
 class UploadFileTest(BaseCase):
-    def test_upload_invalid_file(self):
-        data = dict()
-        data["file"] = "test.png"
-
-        response = self.app.post(
-            f"/upload?id={uuid.uuid4()}",
-            headers={"content-type": "multipart/form-data"},
-            data=data,
-        )
-
-        self.assertEqual(93, len(response.json))
-        self.assertEqual(400, response.status_code)
-
     def test_upload_no_callback_url(self):
         data = dict()
         data["file"] = self.create_test_image()
