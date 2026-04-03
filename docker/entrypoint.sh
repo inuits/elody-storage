65e39edc84fb9dd1414ac206a606d3db8a31d2e5
Tim Standaert <tim.standaert@inuits.eu>
2026-04-03 15:46:21 +0200

    feat: clear /tmp folder on startup for persistent-storage on deployed environments

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -17,11 +17,15 @@ if [ ! -z "$TRUSTED_CA_BUNDLE" ]; then
 fi
 
 if [ "$APP_ENV" = "dev" ]; then
+	echo "Clearing /tmp"
+	rm -rf /tmp/* 2>/dev/null || true
   echo "Starting development server..."
   export FLASK_DEBUG='1'
   cd ~/api
   exec ~/.local/bin/flask run --host=0.0.0.0
 else
+	echo "Clearing /tmp"
+	rm -rf /tmp/* 2>/dev/null || true
   echo "Starting gunicorn server..."
   cd ~/api
   exec ~/.local/bin/gunicorn ${GUNICORN_SSL_CA} -b 0.0.0.0 --timeout 0 "app:app" --access-logfile - --access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(M)sms %(b)s "%(f)s" "%(a)s"'
