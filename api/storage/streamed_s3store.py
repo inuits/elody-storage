31e01a7f78cfd4201ff4a7fe7a5b5333a509e214
Eray Özcan <eray@inuits.eu>
2026-04-09 11:29:04 +0200

    fix: use high level api copy method to handle file copies bigger then 5GB

--- api/storage/streamed_s3store.py
+++ api/storage/streamed_s3store.py
@@ -1,43 +1,42 @@
-from boto3 import client
+from boto3 import client, resource
 from botocore.config import Config
 from contextlib import closing
 from elody.error_codes import ErrorCode, get_error_code, get_write
 from elody.exceptions import DuplicateFileException
 from io import BytesIO
 from mypy_boto3_s3.client import S3Client
+from mypy_boto3_s3.service_resource import S3ServiceResource
 from os import getenv
 
 
 class StreamedS3Store:
     def __init__(self):
-        self.s3: S3Client = client(
+        self._config = Config(
+            signature_version="s3v4", retries={"max_attempts": 10, "mode": "standard"}
+        )
+        self.resource: S3ServiceResource = resource(
             "s3",
             endpoint_url=getenv("MINIO_ENDPOINT"),
             aws_access_key_id=getenv("MINIO_ACCESS_KEY"),
             aws_secret_access_key=getenv("MINIO_SECRET_KEY"),
-            config=Config(
-                signature_version="s3v4",
-                retries={"max_attempts": 10, "mode": "standard"},
-            ),
+            config=self._config,
         )
-        self.s3_public: S3Client = client(
+        self.client: S3Client = self.resource.meta.client
+        self.public_client: S3Client = client(
             "s3",
             endpoint_url=getenv("MINIO_ENDPOINT_EXT", "http://minio.localhost:8000"),
             aws_access_key_id=getenv("MINIO_ACCESS_KEY"),
             aws_secret_access_key=getenv("MINIO_SECRET_KEY"),
-            config=Config(
-                signature_version="s3v4",
-                retries={"max_attempts": 10, "mode": "standard"},
-            ),
+            config=self._config,
         )
 
     def init_stream(self, *, key: str):
-        return self.s3.create_multipart_upload(Bucket=self.__get_bucket(), Key=key)[
+        return self.client.create_multipart_upload(Bucket=self.__get_bucket(), Key=key)[
             "UploadId"
         ]
 
     def sign_chunk(self, *, stream_id: str, key: str, chunk_sequence: int):
-        return self.s3_public.generate_presigned_url(
+        return self.public_client.generate_presigned_url(
             ClientMethod="upload_part",
             Params={
                 "Bucket": self.__get_bucket(),
@@ -57,10 +56,12 @@ class StreamedS3Store:
         file_info: dict,
         legacy_store,
     ):
+        bucket_name = self.__get_bucket()
         filename = f"{file_info['md5sum']}-{file_info['name']}"
+
         try:
             legacy_store.check_file_exists(
-                filename, file_info["md5sum"], bucket_name=self.__get_bucket()
+                filename, file_info["md5sum"], bucket_name=bucket_name
             )
             is_duplicate = False
         except DuplicateFileException:
@@ -74,8 +75,8 @@ class StreamedS3Store:
                 }
                 for chunk_info in chunks_info
             ]
-            self.s3.complete_multipart_upload(
-                Bucket=self.__get_bucket(),
+            self.client.complete_multipart_upload(
+                Bucket=bucket_name,
                 Key=key,
                 UploadId=stream_id,
                 MultipartUpload={
@@ -84,14 +85,12 @@ class StreamedS3Store:
                     )  # pyright: ignore
                 },
             )
-            self.s3.copy_object(
-                Bucket=self.__get_bucket(),
-                Key=filename,
-                CopySource={"Bucket": self.__get_bucket(), "Key": key},
+            self.resource.Bucket(bucket_name).copy(
+                {"Bucket": bucket_name, "Key": key}, filename
             )
-            self.s3.delete_object(Bucket=self.__get_bucket(), Key=key)
+            self.client.delete_object(Bucket=bucket_name, Key=key)
 
-        response = self.s3.get_object(Bucket=self.__get_bucket(), Key=filename)
+        response = self.client.get_object(Bucket=bucket_name, Key=filename)
         with response["Body"] as file_stream:
             header_data = file_stream.read(500 * (1024**2))
             with closing(BytesIO(header_data)) as file_part:
@@ -104,16 +103,16 @@ class StreamedS3Store:
                     parent_job_id=None,
                     md5sum=file_info["md5sum"],
                     skip_s3_upload=True,
-                    bucket_name=self.__get_bucket(),
+                    bucket_name=bucket_name,
                     is_duplicate=is_duplicate,
                 )
 
     def abort_stream(self, *, stream_id: str, key: str):
         try:
-            self.s3.abort_multipart_upload(
+            self.client.abort_multipart_upload(
                 Bucket=self.__get_bucket(), Key=key, UploadId=stream_id
             )
-        except self.s3.exceptions.NoSuchUpload:
+        except self.client.exceptions.NoSuchUpload:
             pass
 
     def __get_bucket(self):
