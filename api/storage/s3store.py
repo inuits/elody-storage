d23cb54cc126f77c5092be242dfa8f9026877f6e
Tim Standaert <tim.standaert@inuits.eu>
2026-07-10 10:09:10 +0200

    Revert "temp: disable alert"
    This reverts commit 88e875df39bccf392ebe3e2f4f7fa6c6748e14e3.

--- api/storage/s3store.py
+++ api/storage/s3store.py
@@ -12,12 +12,7 @@ from botocore.exceptions import ClientError
 from cloudevents.v1.conversion import to_dict
 from cloudevents.v1.http import CloudEvent
 from dateutil import parser
-from elody.error_codes import (
-    ErrorCode,
-    # get_alert,
-    get_error_code,
-    get_write,
-)
+from elody.error_codes import ErrorCode, get_alert, get_error_code, get_write
 from elody.exceptions import (
     DuplicateFileException,
     EmptyFileException,
@@ -523,8 +518,7 @@ class S3StorageManager:
             ):  # cannot detect based on mimetype, since mimetype of all emtpy files is "aplication/x-empty"
                 message = f"File {key} is empty."
                 raise EmptyFileException(
-                    f"{get_error_code(ErrorCode.EMPTY_FILE, 'A-')} {message}",
-                    # f"{get_error_code(ErrorCode.EMPTY_FILE, get_alert())} {message}",
+                    f"{get_error_code(ErrorCode.EMPTY_FILE, get_alert())} {message}",
                     key,
                 )
             raise Exception("Empty file, upload aborted")
